ba934dd070314b61dbcfc4e713e6e7df80637a02
skydrome <chris2603@gmail.com>
2011-03-06 01:20:19 -0500

    blah

--- installer.sh
+++ installer.sh
@@ -19,7 +19,7 @@ VERSION='1.0.0~git'                                              #
 DATE='Feb 21 2011'                                               #
 ##################################################################
 trap ctrl_c SIGINT
-if [[ $(bash --version | head -n1 | cut -c 19-23) > 3 ]]  # Check for bash verion 4+
+if [ `bash --version | head -n1 | cut -c 19` -gt '3' ]  # Check for bash verion 4+
 	then source includes/functions.sh || echo " Error: while loading functions.sh"  # Source in our functions
 	else echo "Please install package: bash, version 4.0 or higher" && exit
 fi
