9159c3342357b0fa235334f4809092de0f5be79d
skydrome <chris2603@gmail.com>
2011-05-23 18:01:42 -0400

    minor fix

--- tools/destroydrive.sh
+++ tools/destroydrive.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ $1 = /dev/??? && -b $1]]; then
+if [[ $1 = /dev/* && -b $1 ]]; then
 	if [[ $(type -P "openssl") && $(type -P "pv") ]]; then
 		openssl enc -aes256 -k "foo" < /dev/urandom | pv -trb > $1
-	else echo -e "OpenSSL and PV need to be installed... \n"
+	else echo -e "Please install OpenSSL and PV and try again... \n"
 	fi
 else echo -e "Please supply a valid hard drive to wipe: EX: /dev/sda \n"
 fi
